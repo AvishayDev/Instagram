36119bbfdd4cb4d6e113856acef4ed07cda87f55
אבישי אליחי <avishayelihay@gmail.com>
2023-11-28 14:28:35 +0200

    add style to the navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import NavBar from './components/Navbar';
+
+
 
 function App() {
   return (
     <div className="App">
-
+      <NavBar/>
     </div>
   );
 }
