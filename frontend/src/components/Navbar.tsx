e6cd3757af59ca86fd0e4010368dd52e5eb1f606
אבישי אליחי <avishayelihay@gmail.com>
2023-11-29 15:24:16 +0200

    remove userId from the path. makenavbar to work.

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,15 +2,26 @@ import {BottomNavigation, BottomNavigationAction} from '@mui/material'
 import HomeIcon from '@mui/icons-material/Home';
 import AddToPhotosIcon from '@mui/icons-material/AddToPhotos';
 import PersonIcon from '@mui/icons-material/Person';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+
+const NavigateValues: string[] = [
+    'feed',
+    'share',
+    'profile'
+]
+
 
 function NavBar() {
     const [value,setValue] = useState(0);
+    const navigate = useNavigate();
     
-    useEffect(()=>{
-        console.log(value)
-    },[value])
-    
+    const handleNavigation = (event:any, newValue:number) => {
+        setValue(newValue)
+        navigate(`/${NavigateValues[newValue]}`)
+    }
+
     return ( 
         <BottomNavigation 
                 sx={{ 
@@ -19,7 +30,7 @@ function NavBar() {
                     borderTop: '#d3d3d3 solid 1px',
                     bottom:0}}
                 value={value}
-                onChange={(event,newValue)=> setValue(newValue)}>
+                onChange={handleNavigation}>
             <BottomNavigationAction label='Feed' icon={<HomeIcon/>}/>
             <BottomNavigationAction label='Share' icon={<AddToPhotosIcon/>}/>
             <BottomNavigationAction label='Profile' icon={<PersonIcon/>}/>
