488cca32f060530a089e5102e3b7267db8c0d240
אבישי אליחי <avishayelihay@gmail.com>
2023-11-30 14:06:01 +0200

    add all the components to create the page.

--- frontend/src/pages/Share.tsx
+++ frontend/src/pages/Share.tsx
@@ -1,5 +1,20 @@
+import { Button, TextField, Typography } from "@mui/material";
+import { Stack } from "@mui/system";
+
 function Share() {
-    return ( <h1>Share</h1> );
+    return ( 
+        <Stack spacing={4} sx={{marginTop:4}}>
+            <Typography variant="h4">Let's Post Something!</Typography>
+            <TextField
+                label='Add Image Url'/>
+            
+            <TextField
+                label='Write Something..'/>
+            <Button variant="contained">
+                Publish!
+            </Button>
+        </Stack>
+        );
 }
 
 export default Share;
