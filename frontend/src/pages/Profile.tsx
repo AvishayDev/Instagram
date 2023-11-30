d6d0b24a3ecb2b1aab4618481158c233a6e39473
אבישי אליחי <avishayelihay@gmail.com>
2023-11-30 14:25:24 +0200

    create header of profile page.

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,5 +1,34 @@
+import { Box, Stack, Typography } from "@mui/material";
+
 function Profile() {
-    return ( <h1>Profile</h1> );
+    return ( 
+
+        <Stack spacing={6} width='100vw'>
+                <Stack spacing={4} alignItems='flex-start' margin={2}>
+                    <Box   
+                            component='div'
+                            sx={{
+                                width:'100px',
+                                height:'100px',
+                                bgcolor:'blue',
+                                borderRadius:'100%'
+                            }}/>
+                    
+                    <Stack alignItems='flex-start'>
+                        <Typography>Fname Lname</Typography>
+                        <Typography>Username</Typography>
+                    </Stack >
+                    
+                    <Stack alignItems='flex-start'>
+                        <Typography>Bio</Typography>
+                        <Typography variant="body2" align="left" sx={{width:'50vw'}}>woenfnf oewfmoi wedwdwd wdwd</Typography>
+                    </Stack>
+                </Stack>
+
+                
+        </Stack>
+        
+        );
 }
 
 export default Profile;
