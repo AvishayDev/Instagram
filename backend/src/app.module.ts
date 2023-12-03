d216584a2c812168895f9039c07195fcdbbe68ee
אבישי אליחי <avishayelihay@gmail.com>
2023-12-03 17:03:41 +0200

    setup entities

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './endpoints/users/users.module';
 import { PostsModule } from './endpoints/posts/posts.module';
 import { LikesModule } from './endpoints/likes/likes.module';
+import { User } from './Tables/User';
+import { Post } from './Tables/Post';
+import { Like } from './Tables/Like';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
@@ -14,7 +17,7 @@ import { LikesModule } from './endpoints/likes/likes.module';
     username:'postgres',
     password:'AvishayDEV19',
     database:'instagram',
-    entities:[],
+    entities:[User,Post,Like],
     synchronize:true,
   }),UsersModule,PostsModule,LikesModule],
   controllers: [AppController],
