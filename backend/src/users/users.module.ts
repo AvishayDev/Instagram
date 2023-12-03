bf60b27d44355a79bd94eaf7c0bee2452f7c0e48
אבישי אליחי <avishayelihay@gmail.com>
2023-12-03 15:54:54 +0200

    move table into endpoint

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './services/users.service';
 import { UsersController } from './controllers/users.controller';
-import { User } from 'src/Tables/User';
+import { User } from 'src/users/User';
 
 
 @Module({
