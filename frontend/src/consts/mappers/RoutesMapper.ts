76a21785e1152ddd67562d85ac40f4c81d82a51a
אבישי אליחי <avishayelihay@gmail.com>
2023-12-03 14:27:01 +0200

    added enum to map the path and page name. apply the mapper in header. done

--- frontend/src/consts/mappers/RoutesMapper.ts
+++
@@ -1,7 +0,0 @@
-
-
-export enum RoutesMapper {
-    '/login'='Login',
-    '/feed'='Feed',
-    '/share'='Share'
-}
---
+++ frontend/src/consts/mappers/RoutesToPagesNameMapper.ts
@@ -0,0 +1,11 @@
+
+
+export enum RoutesToPagesNameMapper {
+    '/login'='Login',
+    '/feed'='Feed',
+    '/share'='Share',
+    '/profile'='Profile',
+    '/register/1'='Login Info',
+    '/register/2'='Private Info',
+    '/register/3'='About You',
+}
