29eea2dd3110ca769126ae6a14a00d36f6b8b413
אבישי אליחי <avishayelihay@gmail.com>
2023-12-05 14:56:35 +0200

    add config for get.env vars

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {config} from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
 
-config();
 async function bootstrap() {
   
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist:true,
-  }));
+  app.useGlobalPipes(new ValidationPipe({ whitelist:true }));
 
   const port = process.env.SERVER_PORT || 3001;
   await app.listen(port);
