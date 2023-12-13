5a4e030372c16aa2f7e664b34697ba492be2ec48
אבישי אליחי <avishayelihay@gmail.com>
2023-12-13 11:31:35 +0200

    add default image to add post

--- frontend/src/pages/Share.tsx
+++ frontend/src/pages/Share.tsx
@@ -1,10 +1,22 @@
-import { Button, TextField, Typography } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
 import { Stack } from "@mui/system";
+import { IMAGES } from "../consts/Images";
 
 function Share() {
     return ( 
         <Stack spacing={4} marginTop={4}>
             <Typography variant="h4">Let's Post Something!</Typography>
+                <Box   
+                    component='img'
+                    sx={{
+                        width:'300px',
+                        height:'300px',
+                        boxShadow:2,
+                        alignSelf:'center'
+                        
+                    }}
+                    src={IMAGES.defaultPostImage}
+                    />
             <TextField
                 label='Image Url'/>
             
