139e43339e841fb2a3471c8f371ab4615242a33c
אבישי אליחי <avishayelihay@gmail.com>
2023-12-14 15:36:53 +0200

    create feed endpoint (with queryBuilder!!)

--- backend/src/endpoints/posts/posts.service.ts
+++ backend/src/endpoints/posts/posts.service.ts
@@ -4,6 +4,7 @@ import { Post } from "src/Tables/Post";
 import { Repository } from "typeorm";
 import { CreatePostDB } from "./dbtypes/CreatePost.db";
 import { UsersService } from "../users/users.service";
+import { Like } from "src/Tables/Like";
 
 
 
@@ -27,15 +28,49 @@ export class PostsService {
         return post
     }
 
-    getPostsByPage(page:number){
-        return this.postsRepository.find({
-            relations:['user','likes'],
-            order:{
-                createdAt: 'DESC'
-            },
-            skip:this.pageSize*page,
-            take:this.pageSize
-        })
+    // getPostsByPage(page:number){
+    //     return this.postsRepository.find({
+    //         relations:['user','likes'],
+    //         order:{
+    //             createdAt: 'DESC'
+    //         },
+    //         skip:this.pageSize*page,
+    //         take:this.pageSize
+    //     })
+    // }
+
+    async getPostsByPage(page:number, userId:number){
+        /**
+            userProfileImageUrl:string
+            userFirstName:string
+            userLastName:string
+            uploadDate:Date
+            ImageUrl:string
+            text:string
+            isLiked:boolean
+            numOfLikes:number}[] = [
+
+         */
+        return await this.postsRepository.createQueryBuilder('post')
+            .select([
+                'user.profileImageUrl AS userProfileImageUrl',
+                'user.firstName AS userFirstName',
+                'user.lastName AS userLastName',
+                'post.createdAt AS uploadDate',
+                'post.imageUrl AS imageUrl',
+                'post.text AS text',
+                'post.id AS postId',
+                'COUNT(like.id) AS likes',
+                `COUNT(CASE WHEN like.user.id = :userId THEN 1 ELSE NULL END) AS isLiked`,
+            ])
+            .leftJoin('post.likes','like')
+            .leftJoin('post.user','user')
+            .groupBy('post.id, user.id')
+            .orderBy('post.createdAt','DESC')
+            .offset(this.pageSize*page)
+            .limit(this.pageSize)
+            .setParameters({ userId })
+            .getRawMany()
     }
 
     async createPost(createPostDB: CreatePostDB){
