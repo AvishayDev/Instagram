8f618184b92bf116661a3ebd36de437fa16c3a57
אבישי אליחי <avishayelihay@gmail.com>
2023-12-24 20:03:00 +0200

    create protected routes. change login-logout functionallity to app.tsx change localStorageHook to clean localStorage keys adjust profile and login to work as sayed above.

---
+++ frontend/src/ProtectedRoute.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from "react";
+import { Navigate, Outlet } from "react-router-dom";
+
+
+interface ProtectedRouteProps {
+    isAllowed:boolean
+    redirectPath:string
+    children:ReactNode
+}
+
+
+function ProtectedRoute(props:ProtectedRouteProps) {
+    
+    const {isAllowed,redirectPath,children} = props;
+    
+    return ( 
+        <>
+            {
+               !isAllowed ?  <Navigate to={redirectPath} replace />
+                : children ? children : <Outlet/>
+            }
+        </>);
+
+}
+
+export default ProtectedRoute;
